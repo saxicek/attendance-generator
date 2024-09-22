b3149a68d68d6b9cca612d4c1e49f3f25855631a
sax <saxicek@gmail.com>
2024-09-22 12:43:35 +0200

    Remove router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {FormComponent} from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormComponent],
+  imports: [FormComponent],
   template: `
     <h1>Generátor docházky</h1>
     <app-form></app-form>
